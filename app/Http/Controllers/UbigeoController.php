4bcc7e179aaeb2d175387c5450ba954179e64865
ddscarlos <dds_carlos@hotmail.com>
2025-01-09 15:59:07 -0500

    CAMBIOS REALIZADOS

--- app/Http/Controllers/UbigeoController.php
+++ app/Http/Controllers/UbigeoController.php
@@ -24,28 +24,66 @@ use Dompdf\Exception as DomException;
 use Dompdf\Options;
 
 
-class GeneralController extends Controller
+class UbigeoController extends Controller
 {
     private static $conexion = 'sqlsrv';
     private static $conexionpsql = 'pgsql';
 
-    public function tipodocumentosel(Request $request)
+    public function departamentosel(Request $request)
     {
         try {
             //PARAMETROS DE ENTRADA
-            $p_tdi_id = $request['p_tdi_id'] ? $request['p_tdi_id'] : 0;
-            $p_tpe_id = $request['p_tpe_id'] ? $request['p_tpe_id'] : 0;
+            $p_ude_id = $request['p_ude_id'] ? $request['p_ude_id'] : 0;
 
             //VALIDACIONES
-            if (is_null($p_tdi_id) || is_null($p_tpe_id)) {
+            if (is_null($p_ude_id)) {
                 return response()->json([
                     'success' => false,
-                    'message' => 'Todos los parámetros son obligatorios: p_loging, p_passwd, p_apl_id.'
+                    'message' => 'Todos los parámetros son obligatorios: p_ude_id.'
                 ], 400);
             }
             //CONEXION
             $results = DB::connection(self::$conexionpsql)->select(
-                'SELECT * FROM master.spu_tipodocide_sel(?, ?)',[$p_tdi_id, $p_tpe_id]
+                'SELECT * FROM ubigeo.spu_ubigeodep_sel(?)',[$p_ude_id]
+            );
+            //RETORNA RESPUESTA
+            return response()->json([
+                'success' => true,
+                'data' => $results
+            ]);
+        //ERROR AL EJECUTAR UNA MALA CONSULTA SQL
+        } catch (\Illuminate\Database\QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error al ejecutar la consulta en la base de datos.',
+                'error' => $e->getMessage()
+            ], 500);
+        //ALCUN OTRO TIPO DE ERROR
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Ocurrió un error inesperado.',
+                //'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+    public function provinciasel(Request $request)
+    {
+        try {
+            //PARAMETROS DE ENTRADA
+            $p_ude_id = $request['p_ude_id'] ? $request['p_ude_id'] : 0;
+            $p_upr_id = $request['p_upr_id'] ? $request['p_upr_id'] : 0;
+
+            //VALIDACIONES
+            if (is_null($p_ude_id) || is_null($p_upr_id)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Todos los parámetros son obligatorios: p_ude_id, p_upr_id.'
+                ], 400);
+            }
+            //CONEXION
+            $results = DB::connection(self::$conexionpsql)->select(
+                'SELECT * FROM ubigeo.spu_ubigeopro_sel(?, ?)',[$p_ude_id, $p_upr_id]
             );
             //RETORNA RESPUESTA
             return response()->json([
@@ -68,64 +106,23 @@ class GeneralController extends Controller
             ], 500);
         }
     }
-
-    public function redsocialsel(Request $request)
+    public function distritosel(Request $request)
     {
         try {
             //PARAMETROS DE ENTRADA
-            $p_rso_id = $request['p_rso_id'] ? $request['p_rso_id'] : 0;
-            $p_rso_activo = $request['p_rso_activo'] ? $request['p_rso_activo'] : 0;
+            $p_upr_id = $request['p_upr_id'] ? $request['p_upr_id'] : 0;
+            $p_udi_id = $request['p_udi_id'] ? $request['p_udi_id'] : 0;
 
             //VALIDACIONES
-            if (is_null($p_rso_id) || is_null($p_rso_activo)) {
+            if (is_null($p_upr_id) || is_null($p_udi_id)) {
                 return response()->json([
                     'success' => false,
-                    'message' => 'Todos los parámetros son obligatorios: p_rso_id, p_rso_activo.'
+                    'message' => 'Todos los parámetros son obligatorios: p_upr_id, p_udi_id.'
                 ], 400);
             }
             //CONEXION
             $results = DB::connection(self::$conexionpsql)->select(
-                'SELECT * FROM master.spu_redsocial_sel(?, ?)',[$p_rso_id, $p_rso_activo]
-            );
-            //RETORNA RESPUESTA
-            return response()->json([
-                'success' => true,
-                'data' => $results
-            ]);
-        //ERROR AL EJECUTAR UNA MALA CONSULTA SQL
-        } catch (\Illuminate\Database\QueryException $e) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Error al ejecutar la consulta en la base de datos.',
-                //'error' => $e->getMessage()
-            ], 500);
-        //ALCUN OTRO TIPO DE ERROR
-        } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Ocurrió un error inesperado.',
-                //'error' => $e->getMessage()
-            ], 500);
-        }
-    }
-
-    public function estadossel(Request $request)
-    {
-        try {
-            //PARAMETROS DE ENTRADA
-            $p_est_id = $request['p_est_id'] ? $request['p_est_id'] : 0;
-            $p_est_activo = $request['p_est_activo'] ? $request['p_est_activo'] : 0;
-
-            //VALIDACIONES
-            if (is_null($p_est_id) || is_null($p_est_activo)) {
-                return response()->json([
-                    'success' => false,
-                    'message' => 'Todos los parámetros son obligatorios: p_est_id, p_est_activo.'
-                ], 400);
-            }
-            //CONEXION
-            $results = DB::connection(self::$conexionpsql)->select(
-                'SELECT * FROM arquitracking.spu_estado_sel(?, ?)',[$p_est_id, $p_est_activo]
+                'SELECT * FROM ubigeo.spu_ubigeodis_sel(?, ?)',[$p_upr_id, $p_udi_id]
             );
             //RETORNA RESPUESTA
             return response()->json([
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\EmpresaController;
 use App\Http\Controllers\GeneralController;
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\UbigeoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,13 +12,20 @@ Route::get('/', function () {
 
 Route::prefix('v1')->group(function () {
     Route::prefix('empresa')->group(function () {
-        //Route::post('eventos/eliminararchivos', [CalendarioController::class, 'eliminararchivos']);
         Route::post('listar', [EmpresaController::class, 'sel']);
         Route::post('ins', [EmpresaController::class, 'ins']);
     });
 
     Route::prefix('general')->group(function () {
         Route::post('tipo-documento/listar', [GeneralController::class, 'tipodocumentosel']);
+        Route::post('red-social/listar', [GeneralController::class, 'redsocialsel']);
+        Route::post('estados/listar', [GeneralController::class, 'estadossel']);
+    });
+
+    Route::prefix('ubigeo')->group(function () {
+        Route::post('departamento/listar', [UbigeoController::class, 'departamentosel']);
+        Route::post('provincia/listar', [UbigeoController::class, 'provinciasel']);
+        Route::post('distrito/listar', [UbigeoController::class, 'distritosel']);
     });
 
 });
